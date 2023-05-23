fa4af8390b65a2334109773e934319f2ce9813a5
Ciki7 <130870826+Ciki7@users.noreply.github.com>
2023-05-23 15:42:53 +0200

    Create header.h

---
+++ header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+void displaySubmenuViewPasswords();
+void displaySubmenuAddPassword();
+void displaySubmenuGeneratePassword();
+void displaySubmenuEditPassword();
+void displaySubmenuDeletePassword();
+void displaySubmenuChangeMasterPassword();
+void displaySubmenuHelp();
+
+#endif  //HEADER_H
