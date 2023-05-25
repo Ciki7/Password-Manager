0fcd2628b97f3981f1dab18e17318b529b3fce0a
Ciki7 <130870826+Ciki7@users.noreply.github.com>
2023-05-25 10:11:58 +0200

    Update header.h

--- header.h
+++ header.h
@@ -8,5 +8,19 @@ void displaySubmenuEditPassword();
 void displaySubmenuDeletePassword();
 void displaySubmenuChangeMasterPassword();
 void displaySubmenuHelp();
+void displayInstructions();
+void displayDescription();
+void addPassword();
+
+
+typedef struct {
+    char name[100];
+    char password[100];
+} ACCOUNT;
+
+typedef struct {
+    char username[100];
+    char password[100];
+} PASSWORD;
 
 #endif  //HEADER_H
