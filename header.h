c594519e69019899bcd599d6964f7409c3f598ea
Ciki7 <130870826+Ciki7@users.noreply.github.com>
2023-06-01 10:14:05 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,26 +1,35 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-void displaySubmenuViewPasswords();
-void displaySubmenuAddPassword();
-void displaySubmenuGeneratePassword();
-void displaySubmenuEditPassword();
-void displaySubmenuDeletePassword();
-void displaySubmenuChangeMasterPassword();
-void displaySubmenuHelp();
-void displayInstructions();
-void displayDescription();
-void addPassword();
-
-
-typedef struct {
-    char name[100];
-    char password[100];
-} ACCOUNT;
+#define MAX_PASSWORDS 100
+#define FILENAME "passwords.txt"
+#define MASTER_PASSWORD_LENGTH 100
+#define MASTER_PASSWORD_FILE "master_password.txt"
 
 typedef struct {
     char username[100];
     char password[100];
 } PASSWORD;
 
-#endif  //HEADER_H
+void displaySubmenuViewPasswords();
+void displaySubmenuAddPassword();
+void displaySubmenuEditPassword();
+void displaySubmenuDeletePassword();
+void displaySubmenuChangeMasterPassword();
+void displaySubmenuHelp();
+void displayInstructions();
+void displayDescription();
+void addPassword(PASSWORD passwords[], int* numPasswords);
+void savePasswords(PASSWORD passwords[], int numPasswords);
+void displayPasswords(PASSWORD passwords[], int numPasswords);
+void loadPasswords(PASSWORD passwords[], int* numPasswords);
+void editPassword(PASSWORD passwords[], int numPasswords);
+void requestMasterPassword();
+int isMasterPasswordSet();
+void setMasterPassword(const char* password);
+void changeMasterPassword();
+void findPassUsername(const PASSWORD passwords[], int numPasswords, const char* username);
+
+
+
+#endif  // HEADER_H
