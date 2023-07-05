ee93659f1ba4e9b1c3d16709ebd1166c83697aeb
Ciki7 <130870826+Ciki7@users.noreply.github.com>
2023-07-06 01:48:40 +0200

    Update header.h

--- header.h
+++ header.h
@@ -7,10 +7,12 @@
 #define MAX_PASSWORD_LENGTH 100
 #define MASTER_PASSWORD_FILE "master_password.txt"
 
+
 typedef struct {
     char username[100];
     char password[100];
 } PASSWORD;
+
 
 void displaySubmenuViewPasswords();
 void displaySubmenuAddPassword();
@@ -23,17 +25,14 @@ void displayDescription();
 void addPassword(PASSWORD passwords[], int* numPasswords);
 void savePasswords(PASSWORD passwords[], int numPasswords);
 void displayPasswords(PASSWORD passwords[], int numPasswords);
-void loadPasswords(PASSWORD passwords[], int* numPasswords);
-void editPassword(PASSWORD passwords[], int numPasswords);
+void editPassword(const char* filename);
 void requestMasterPassword();
 int isMasterPasswordSet();
 void setMasterPassword(const char* password);
 void changeMasterPassword();
-void findPassUsername(const PASSWORD passwords[], int numPasswords, const char* username);
-void generateRandomPassword();
-void generatePasswordWithOptions();
-void deletePassword(PASSWORD passwords[], int* numPasswords);
-void deleteMasterPassword();
-
+void searchPasswords(const char* username);
+void deletePassword(const char* username);
+int validatePassword(const char* password);
+int comparePasswords(const void* a, const void* b);
 
 #endif  // HEADER_H
